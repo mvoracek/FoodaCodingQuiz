42b6ffb888da39b1123eb318f483f0e7ad0ad019
mjvoracek <mjvoracek@gmail.com>
2014-03-14 10:47:41 -0500

    Initial Commit

---
+++ FoodaCodingQuiz/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FoodaCodingQuiz
+//
+//  Created by Matthew Voracek on 3/14/14.
+//  Copyright (c) 2014 Matthew Voracek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ FoodaCodingQuizTests/FoodaCodingQuizTests.m
@@ -0,0 +1,34 @@
+//
+//  FoodaCodingQuizTests.m
+//  FoodaCodingQuizTests
+//
+//  Created by Matthew Voracek on 3/14/14.
+//  Copyright (c) 2014 Matthew Voracek. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FoodaCodingQuizTests : XCTestCase
+
+@end
+
+@implementation FoodaCodingQuizTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
